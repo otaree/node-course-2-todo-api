c1c0a6560288a9968d7b55e21e0a11ff258cb4c2
otaree <deep.yen.ur.hash@gmail.com>
2018-02-19 11:20:26 +0530

    Add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -17,6 +17,7 @@ var {
 var {
     User
 } = require('./models/user');
+var { authenticate } = require('./middleware/authenticate');
 
 const port = process.env.PORT;
 
@@ -123,6 +124,11 @@ app.post('/users', (req, res) => {
     }).catch((e) => {
         res.status(400).send(e);
     });
+});
+
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 app.listen(port, () => {
